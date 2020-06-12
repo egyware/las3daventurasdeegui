32cc67c760b3782043aa3507925154d06f882e2a
egyware <egyware@gmail.com>
2020-06-11 20:02:31 -0400

    Pequenio error
    :D

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const mysql = require('mysql');
 var app = express();
 
+
+app.use(express.json())
+app.use(express.urlencoded())
+
 //constantes
 const port = process.env.PORT || 1337;
 var connectionString = process.env.MYSQLCONNSTR_localdb || 'mysql://egui:passwd@localhost/las3daventuras';
@@ -20,7 +24,7 @@ var pool = mysql.createPool(connectionString);
 
 //codigo
 app.use(express.static('public'));
-app.use('/api/proveedores', function(req, res){
+app.get('/api/proveedores', function(req, res){
     pool.getConnection(function(err, connection) {
         if (err) {
           console.error('error connecting: ' + err.stack);
@@ -30,10 +34,23 @@ app.use('/api/proveedores', function(req, res){
         connection.query('SELECT * FROM Proveedores', function (error, results, fields) {
             if (error) throw error;
             // connected!
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify(results));   
+            res.json(results);   
 
             connection.release();
+        });
+    });
+});
+app.post('/api/proveedores', function(req, res){
+    pool.getConnection(function(err, connection) {
+        if (err) {
+          console.error('error connecting: ' + err.stack);
+          return;
+        }      
+        console.log('connected as id ' + connection.threadId); 
+        connection.query('INSERT INTO `proveedores` (`website`, `empresa`, `descripcion`) VALUES(?, ?, ?)',[req.body.website, req.body.nombre, req.body.descripcion], function (error, results){
+            if (error) throw error;
+            // connected!
+            res.redirect('/');
         });
     });
 });
