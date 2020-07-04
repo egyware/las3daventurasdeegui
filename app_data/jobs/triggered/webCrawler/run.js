ee04efab392e189200180df65f1676421fa1fdfe
egyware <egyware@gmail.com>
2020-07-04 00:15:50 -0400

    Refactorización
    - Hice un poco de abuso del then y await

--- app_data/jobs/triggered/webCrawler/run.js
+++ app_data/jobs/triggered/webCrawler/run.js
@@ -233,7 +233,15 @@ async function crawler(){
             });
             await Q.allSettled(promesas);
         })
-        .then(async function(){            
+        .then(async function(){
+            let tokens = null;
+            await db.query(`select token from subscriptores`)
+            .then(function(results) {
+                tokens = results.map(currentValue => currentValue.token);
+            })
+            return tokens;
+        })
+        .then(function(tokens){     
             if(notificaciones.length > 0){
                 let notificacionesProveedores = groupBy(notificaciones, 'proveedorId');
                 for (let [proveedorId, notificacionesProveedor] of Object.entries(notificacionesProveedores)) {                    
@@ -253,7 +261,7 @@ async function crawler(){
                                     link: `https://las3daventurasdeegui.azurewebsites.net/proveedor/${proveedorId}`
                                 }
                             },
-                            tokens: null
+                            tokens: tokens
                         };
                         
                         if(notificaciones.length == 1) //si hay una sola, reemplazamos la notificacion por defecto
@@ -261,52 +269,38 @@ async function crawler(){
                             message.notification.body = notificaciones[0].descripcion;
                         }
 
+                        return message;                        
+                    } else { //si hay varios tipos                        
+                        let notificacionesTipo = notificacionesTipos[0];
+                        //let tipo           = notificacionesTipo[0];
+                        let notificaciones = notificacionesTipo[1];                            
+                        let message = {
+                            notification: {
+                                title: `HAY NOVEDADES EN ${notificaciones[0].proveedorNombre}`,
+                                body: `LLegó más filamento? o hay rebajas? haz click acá para enterarte`
+                            },
+                            webpush: {
+                                fcm_options: {
+                                    link: `https://las3daventurasdeegui.azurewebsites.net/stock/${proveedorId}`
+                                }
+                            },
+                            tokens: tokens
+                        };
                         
-                        await db.query(`select token from subscriptores`)
-                        .then(async function(results){
-                            message.tokens = results.map(currentValue => currentValue.token);                            
-                            await admin.messaging().sendMulticast(message)
-                            .then((response) => {
-                                // Response is a message ID string.
-                                console.log('Successfully sent message:', response);
-                            })
-                            .catch((error) => {
-                                console.log('Error sending message:', error);
-                            })                                                
-                        })
-                        
-                    } else { //si hay varios tipos
-                            let notificacionesTipo = notificacionesTipos[0];
-                            //let tipo           = notificacionesTipo[0];
-                            let notificaciones = notificacionesTipo[1];                            
-                            let message = {
-                                notification: {
-                                    title: `HAY NOVEDADES EN ${notificaciones[0].proveedorNombre}`,
-                                    body: `LLegó más filamento? o hay rebajas? haz click acá para enterarte`
-                                },
-                                webpush: {
-                                    fcm_options: {
-                                        link: `https://las3daventurasdeegui.azurewebsites.net/stock/${proveedorId}`
-                                    }
-                                },
-                                tokens: null
-                            };
-
-                             db.query(`select token from subscriptores`)
-                            .then(async function(results){
-                                message.tokens = results.map(currentValue => currentValue.token);                                
-                                await admin.messaging().sendMulticast(message)
-                                .then((response) => {
-                                    // Response is a message ID string.
-                                    console.log('Successfully sent message:', response);
-                                })
-                                .catch((error) => {
-                                    console.log('Error sending message:', error);
-                                });                                
-                            })
+                        return message;                                              
                     }                  
                 };                
             }
+        })
+        .then(async function(message){
+            await admin.messaging().sendMulticast(message)
+                .then((response) => {
+                    // Response is a message ID string.
+                    console.log('Successfully sent message:', response);
+                })
+                .catch((error) => {
+                    console.log('Error sending message:', error);
+                })
         })
         .catch(console.log.bind(console))        
         .done(function(){            
